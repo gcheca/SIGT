28bec13cd7a0c85f920ad96b78e2d7f081995242
gcheca <gcheca5@alumno.uned.es>
2017-04-24 11:40:36 +0200

    Create UsuarioSistema.java
    Abstract Class about system users from the SGIT

---
+++ UsuarioSistema.java
@@ -0,0 +1,14 @@
+public abstract class UsuarioSistema
+
+{
+    // El unico atributo padre será un id de usuario dentro del sistema
+    private String Idsys;
+
+    /**
+     * Constructor for objects of class UsuarioSistema
+     */
+    public UsuarioSistema()
+    {
+     
+    }
+}
