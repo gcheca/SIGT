101e0ede5b0e45a5b6b075874f40337c1d30cf66
gcheca <gcheca5@alumno.uned.es>
2017-04-24 11:52:29 +0200

    Update UsuarioSistema.java

--- UsuarioSistema.java
+++ UsuarioSistema.java
@@ -2,13 +2,17 @@ public abstract class UsuarioSistema
 
 {
     // El unico atributo padre será un id de usuario dentro del sistema
-    private String Idsys;
+    private String idsys;
 
-    /**
-     * Constructor for objects of class UsuarioSistema
-     */
+    //Constructor por parámetros para UsuarioSistema. Será DNI/CIF sin letra
+    public UsuarioSistema(String idsys)
+    {
+        this.idsys=idsys;        
+    }
+    //Constructor default para UsuarioSistema
     public UsuarioSistema()
     {
-     
+        this.idsys=new String("xxxxxxxx");
     }
 }
+
