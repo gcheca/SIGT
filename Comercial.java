15602901471356cc35d40ddc981cc321a3c6ae65
gcheca <gcheca5@alumno.uned.es>
2017-04-25 20:27:06 +0200

    Create Comercial.java

---
+++ Comercial.java
@@ -0,0 +1,25 @@
+public class Comercial extends Empleados
+{
+    // Variable departamento
+    private String dpto;
+    
+    /**
+     * Constructor for objects of class Comercial
+     */
+    public Comercial(String id, String nombre, String apellido)
+    {
+        super(id, nombre, apellido);
+        this.dpto=new String("Comercial");
+    }
+    
+    //toString
+    /*public String toString()
+    {
+        String e="Empleado: ";
+        e=e.concat(this.id+", ");
+        e=e.concat(this.nombre+", ");
+        e=e.concat(this.apellido+", ");
+        e=e.concat(this.dpto+".\n");
+        return e;
+    }*/
+}
