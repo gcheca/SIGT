066dd3754f542659e90d7e5e084bca0ec7dda092
gcheca <gcheca5@alumno.uned.es>
2017-04-25 21:19:16 +0200

    Create Vehiculo.java

---
+++ Vehiculo.java
@@ -0,0 +1,31 @@
+package GestionVehiculos
+public abstract class Vehiculo
+{
+    // instance variables - replace the example below with your own
+    private String matricula;
+
+    //Constructor por Parámetros
+    public Vehiculo(String matricula)
+    {
+        this.matricula=matricula;
+    }
+    //Constructor default
+    public Vehiculo()
+    {
+        this.matricula=new String ("****XXX");
+    }
+    
+     /**
+     * Getter & Setter de Matricula
+     */
+    public String getmatricuka()
+    {
+        return matricula;
+    }
+    
+    public void setmatricula(String matricula)
+    {
+        this.matricula=matricula;
+    }
+   
+}
