59117a48f7784c32575c0977137a18d15cada306
gcheca <gcheca5@alumno.uned.es>
2017-04-25 20:49:16 +0200

    Create JefeTaller.java

---
+++ JefeTaller.java
@@ -0,0 +1,22 @@
+public class JefeTaller extends Empleados
+{
+    // instance variables - replace the example below with your own
+    private String dpto;
+    
+    /**
+     * Constructor for objects of class JefeTaller
+     */
+    public JefeTaller(String id, String nombre, String apellido, String dpto)
+    {
+        super(id, nombre, apellido);
+        this.dpto=new String ("Jefe de Taller");
+    }
+  
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+    public int Method(int y)
+    {
+        
+    }*/
+}
