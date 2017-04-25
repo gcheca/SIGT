e4bbe9bc84e8460763c0419c3ab9e31b11f0fdd4
gcheca <gcheca5@alumno.uned.es>
2017-04-25 20:55:42 +0200

    Update and rename Particular.class to Particular.java

--- Particular.java
+++ Particular.java
@@ -1,22 +1,25 @@
+import java.lang.String;
 public class Particular extends Clientes
 {
     //Además de las heredadas. Nombre, Apellidos, NIF
-    private String nombre;
-    private String apellido;
-    private String nif;
+    String nombre;
+    String apellido;
+    String nif;
 
     //Contructor por parámetros de Particular
-    public Particular(String nombre, String apellido, String nif)
+    public Particular(String id, String telf, String email, String nombre, String apellido, String nif)
     {
+        super(id, telf, email);
         this.nombre=nombre;
         this.apellido=apellido;
         this.nif=nif;
     }
     //Constructor default de Particular
-    public Particular()
+    /*public Particular()
     {
+        super(id, telf, email);
         this.nombre="Nombre";
         this.apellido="Apellido";
         this.nif="xxxxxxxx";
-    }
+    }*/
 }
