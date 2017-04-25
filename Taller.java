8c8ac5696b8e6a054027d9d12d34248d834b5818
gcheca <gcheca5@alumno.uned.es>
2017-04-25 19:58:39 +0200

    Clase principal

---
+++ Taller.java
@@ -0,0 +1,16 @@
+//Clase iniciadora
+public class Taller
+{
+    public static void main (String [ ] args)
+    {
+        System.out.println ("Clase Taller Ppal");
+        System.out.println ("Interfaz textual");
+    }
+    /**
+     * Constructor for objects of class Taller
+     */
+    public Taller()
+    {
+    }
+
+}
