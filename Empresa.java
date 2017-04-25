06655d9bfef1bea67bfce21ed83629008ef0409e
gcheca <gcheca5@alumno.uned.es>
2017-04-25 20:57:40 +0200

    Update and rename Empresa.class to Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -2,19 +2,21 @@ public class Empresa extends Clientes
 {
     // Además de las heredadas. Meteremos el CIF 
     //que será igual al Id pero sin letra y el nombre de la empresa
-    private String nombreempresa;
-    private String cif;
+    String nombreempresa;
+    String cif;
     
     //Constructor por parámetros para empresa
-    public Empresa(String nombreempresa, String cif)
+    public Empresa(String id, String telf, String email, String nombreempresa, String cif)
     {
+      super(id, telf, email);
       this.nombreempresa=nombreempresa;
       this.cif=cif;
     }
+    
     //Constructor default para empresa
-    public Empresa()
+    /*public Empresa()
     {
-        this.nombreempresa="Nueva empresa";
-        this.cif="xxxxxxxx";       
-    }
+        this.nombreempresa=new String ("Nueva empresa");
+        this.cif=new String ("xxxxxxxx");       
+    }*/
 }
